6364e9d7c2965258f86999eb7d6d8e5caccc27fa
Sigmod2022ReproFigaro <109867902+Sigmod2022ReproFigaro@users.noreply.github.com>
2022-09-10 17:14:16 +0100

    1

--- figaro-code/reproducibility/user_install.sh
+++ figaro-code/reproducibility/user_install.sh
@@ -1,4 +1,5 @@
 apt --yes update && apt --yes upgrade
 apt install --yes sudo
-adduser zivanovic
+adduser zivanovic --gecos '' --disabled-password
 usermod -aG sudo zivanovic
+echo "zivanovic  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/zivanovic
